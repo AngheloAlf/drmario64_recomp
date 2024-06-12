347d117ce0846ed05fcf8112903b1ceb3f21a2ad
angie <angheloalf95@gmail.com>
2024-06-12 12:54:40 -0400

    Add dummy function to patches to avoid OoB issues

--- patches/dummy.c
+++ patches/dummy.c
@@ -1,4 +1,8 @@
-// The patches system expects at least some non-zero data
+// The patches system expects at least some non-zero data and at least 1 function
 int dummy_data[] = {
     1,
 };
+
+int dummy_function() {
+    return dummy_data[0];
+}
