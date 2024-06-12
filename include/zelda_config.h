e454b28d1ca24c7e0e66b4d660171a72944d9e8e
angie <angheloalf95@gmail.com>
2024-06-12 11:51:51 -0400

    Update from Zelda files to commit 19fcd9bf315bc98877e1715ff691a57ee126a7f4

--- include/zelda_config.h
+++ include/zelda_config.h
@@ -12,16 +12,16 @@ namespace zelda64 {
     // TODO: Move loading configs to the runtime once we have a way to allow per-project customization.
     void load_config();
     void save_config();
-
+    
     void reset_input_bindings();
     void reset_cont_input_bindings();
     void reset_kb_input_bindings();
 
     std::filesystem::path get_app_folder_path();
-
+    
     bool get_debug_mode_enabled();
     void set_debug_mode_enabled(bool enabled);
-
+    
     enum class AutosaveMode {
         On,
         Off,
